c67c53800d4941c3c3a6a67bef604a77798d29b3
Johanne-Holst <johanne.holst.klaeboe@cegal.com>
2023-11-11 12:37:36 +0100

    started with reading csv file

--- brreg-case/Program.cs
+++ brreg-case/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main()
+    {
+        string filePath = Path.Combine(@"..\..\..\files\po-kunder.csv");
+
+        using (var reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+                var values = line.Split(';');
+
+                foreach (var value in values)
+                {
+                    Console.Write(value + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
